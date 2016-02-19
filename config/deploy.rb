9b2cc2084acb3a9507dead5adfbb6e05a4e316aa
August Miller <hello@gusmiller.com>
2016-02-19 11:00:59 -0800

    Relocate linked_dirs definition

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,17 +28,18 @@ set :log_level, :info
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'content',
   'site/accounts',
+  'site/cache',
   'thumbs',
   'assets/avatars',
   'assets/css',
   'assets/js'
 )
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
