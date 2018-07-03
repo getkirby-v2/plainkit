dfec0c8e5714c59bb7e8ee86b7d0a39562ddfd13
Bastian Allgeier <mail@bastianallgeier.com>
2018-07-03 11:40:53 +0200

    Update to Kirby 2.5.11

--- panel/app/src/panel.php
+++ panel/app/src/panel.php
@@ -35,13 +35,13 @@ use Kirby\Panel\Models\Page\Blueprint as PageBlueprint;
 
 class Panel {
 
-  static public $version = '2.5.10';
+  static public $version = '2.5.11';
 
   // minimal requirements
   static public $requires = array(
-    'php'     => '5.4.0',
-    'toolkit' => '2.5.10',
-    'kirby'   => '2.5.10'
+    'php'     => '5.6.0',
+    'toolkit' => '2.5.11',
+    'kirby'   => '2.5.11'
   );
 
   static public $instance;
